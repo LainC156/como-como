c20d51f900107f360f341b60ad82f1487e411264
Giovanni Trejo Abasolo <gtaa7x@gmail.com>
2020-10-03 19:27:34 -0500

    cards in home route

--- resources/views/layouts/headers/cards.blade.php
+++ resources/views/layouts/headers/cards.blade.php
@@ -3,7 +3,7 @@
         <div class="header-body">
             <!-- Card stats -->
             <div class="row">
-                @if($user->active)
+                @if($user->active == 1 || $user->trial_status == 1)
                 <div class="col-xl-3 col-lg-6">
                     <div class="card card-stats mb-4 mb-xl-0">
                         <div class="card-body">
